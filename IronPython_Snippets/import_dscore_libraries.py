67f0865bf698aa55b9f6ff915ba041638e7f66a7
Raja <argram30@gmail.com>
2020-01-16 15:01:25 +0100

    Updated 20200116

--- IronPython_Snippets/import_dscore_libraries.py
+++ IronPython_Snippets/import_dscore_libraries.py
@@ -8,3 +8,8 @@ from Autodesk.DesignScript.Geometry import *
 #Importing DSCore Nodes - Colors, Lists everything from Design script could be used in python
 clr.AddReference('DSCoreNodes')
 from DSCore import *
+
+#Import Dynamo Dictionary in to Iron python
+clr.AddReference("DesignScriptBuiltin")
+from DesignScript.Builtin import Dictionary
+OUT = Dictionary.ByKeysValues(["A"], [1,2])
