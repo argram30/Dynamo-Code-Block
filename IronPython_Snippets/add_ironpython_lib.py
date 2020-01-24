1de721a1932fe8de14edfe9a85505da5df5e6e36
Raja <argram30@gmail.com>
2020-01-24 16:04:35 +0100

    Create add_ironpython_lib.py

---
+++ IronPython_Snippets/add_ironpython_lib.py
@@ -0,0 +1,7 @@
+#How to use the Ironpython Libraries by adding it to the path
+#http://www.ironpython.info/index.php?title=Using_the_Python_Standard_Library
+
+import sys
+sys.path.append(r"C:\Program Files (x86)\IronPython 2.7\Lib")
+
+import json
