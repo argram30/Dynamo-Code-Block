ebedfcaf2f4787a22fdaae7a751ac18a9b471434
Raja <argram30@gmail.com>
2020-01-24 17:06:31 +0100

    Update add_ironpython_lib.py

--- IronPython_Snippets/add_ironpython_lib.py
+++ IronPython_Snippets/add_ironpython_lib.py
@@ -4,18 +4,3 @@
 import sys
 sys.path.append(r"C:\Program Files (x86)\IronPython 2.7\Lib")
 import json
-
-#example to merge two with same keys
-eng = IN[0]
-ger = IN[1]
-
-en_dicts = json.loads(eng)
-de_dicts = json.loads(ger)
-
-for key,value in en_dicts.items():
-	new_value = de_dicts[key]
-	new_name = new_value["Name"]
-	en_dicts[key].update({"NewName": new_name})
-	
-data = json.dumps(en_dicts, ensure_ascii=False)
-OUT = data
