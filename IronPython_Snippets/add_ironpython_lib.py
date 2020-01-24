fdbcd548d6148e0d5ffc84788e908330180d4c07
Raja <argram30@gmail.com>
2020-01-24 16:47:44 +0100

    Update add_ironpython_lib.py
    Example to use JSON module to update two libraries

--- IronPython_Snippets/add_ironpython_lib.py
+++ IronPython_Snippets/add_ironpython_lib.py
@@ -15,7 +15,7 @@ de_dicts = json.loads(ger)
 for key,value in en_dicts.items():
 	new_value = de_dicts[key]
 	new_name = new_value["Name"]
-	en_dicts[key].update({"DeutschName": new_name})
+	en_dicts[key].update({"NewName": new_name})
 	
 data = json.dumps(en_dicts, ensure_ascii=False)
 OUT = data
