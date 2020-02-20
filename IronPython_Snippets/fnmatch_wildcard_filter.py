24a7508f1c53735a485e49dea9281c03d67d5cbc
Raja <argram30@gmail.com>
2020-02-20 20:32:31 +0100

    Update fnmatch_wildcard_filter.py

--- IronPython_Snippets/fnmatch_wildcard_filter.py
+++ IronPython_Snippets/fnmatch_wildcard_filter.py
@@ -3,6 +3,5 @@ sys.path.append(r"C:\Program Files (x86)\IronPython 2.7\Lib")
 import fnmatch
 
 data = IN[0]
-output = []
 
-OUT = fnmatch.filter(data, 'gg_???_TU_*')
+OUT = fnmatch.filter(data, '???_*')
